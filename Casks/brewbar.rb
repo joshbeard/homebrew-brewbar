33b45b430df1215eaabcf505556e906aa5529644
GitHub Action <github-actions@github.com>
2025-04-14 02:58:31 +0000

    Update brewbar to 0.0.1

--- Casks/brewbar.rb
+++ Casks/brewbar.rb
@@ -1,6 +1,6 @@
 cask "brewbar" do
   version "0.0.1"
-  sha256 "e1856386d39a8e2eba490f619d1616a0d9fc793ba3a86d5855ba53003c505ba0"
+  sha256 "1881307a703b0998ce5372d0c35ad1717430ff1bdae6850738e2abaea852a559"
 
   url "https://github.com/joshbeard/BrewBar/releases/download/v#{version}/BrewBar.zip"
   name "BrewBar"
@@ -8,6 +8,13 @@ cask "brewbar" do
   homepage "https://github.com/joshbeard/BrewBar"
 
   app "BrewBar.app"
+
+  postflight do
+    set_ownership ["/Applications/BrewBar.app"]
+    system_command "/usr/bin/xattr",
+                  args: ["-cr", "/Applications/BrewBar.app"],
+                  sudo: false
+  end
 
   zap trash: [
     "~/Library/Application Support/BrewBar",
